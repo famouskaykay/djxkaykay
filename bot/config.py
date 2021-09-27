dbb99e73065064d32d407bf5bf30ad6f89192e12
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-27 01:06:02 +0000

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,11 +9,11 @@ AUDIO_CALL = {}
 VIDEO_CALL = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "1995793533:AAESBMKOqi9exREz_DeW3kw8ijsMWhUMmKM")
 SESSION_STRING = getenv("SESSION_STRING", "")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "SafoTheBot")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AsmSafone")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "MyVideoPlayer")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "KayAspirerProject")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "KayAspirerProject")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "userrr0001")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 REPLY_MESSAGE = getenv("REPLY_MESSAGE", "")
 if REPLY_MESSAGE:
