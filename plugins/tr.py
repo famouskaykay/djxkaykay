358d9fc4e48ffd5240a07e322fb07186b7f7ade1
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-28 20:05:32 +0000

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -3,5 +3,5 @@ from config import ASSISTANT_NAME
 
 HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/stream` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stopstream`  **: Stop the stream**"
 START_TEXT = '👋 `Hey My Name Is` **xxxxx player** \n\n`Lets Enjoy Cinematic View  of video chats using several methods from xxxxxx.'
-DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME} `Bot.`"f Group Video Player With Your Friends ❤️😊`'
-ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group by 𝕜𝕒𝕪𝕜𝕒𝕪
+DEVS_TEXT =f"**CREDIT VC VIDEO PLAYER** \n\n`Here Some Developers Helping in Making The` @{ASSISTANT_NAME}  ❤️😊`'"
+ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group by 𝕜𝕒𝕪𝕜𝕒𝕪'
