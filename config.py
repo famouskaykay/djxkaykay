2c2e02a23c094bcd16ce1fa18f5c380a9ca65978
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-28 17:38:48 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,37 @@
-# EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
+import os
 
-# Please edit 'CONFIG = False' to 'CONFIG = True'
-CONFIG = False
+from os import getenv
+from dotenv import load_dotenv
 
-# Add your APP ID from my.telegram.org
-API_ID = 8511571
+load_dotenv()
+admins = {}
+ADMIN = int(os.getenv('ADMIN',import os
 
-# Add your API HASH from my.telegram.org
-API_HASH = ""
+from os import getenv
+from dotenv import load_dotenv
 
-# Read readme and generate string session. Then add that to here.
-SESSION = ""
+load_dotenv()
+admins = {}
+ADMIN = int(os.getenv('ADMIN',1917528355))
+CHANNEL = int(os.getenv('CHANNEL',-1001576388235))
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "xkaykaybot")
+ASSISTANT_NAME = os.getenv("ASSISTANT_NAME", "𝕜𝕒𝕪𝕜𝕒𝕪")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "KayAspirerProject")
+UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "kayAspirerProject")
+SOURCE_CODE = os.getenv("SOURCE_CODE", "github.com/famouskaykay/djxkaykay")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION_NAME = os.getenv("SESSION_NAME")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))))
+CHANNEL = int(os.getenv('CHANNEL',12345))
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "VcVideoRobot")
+ASSISTANT_NAME = os.getenv("ASSISTANT_NAME", "Vc Video Player")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "DeeCodeSupport")
+UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "DeeCodeBots")
+SOURCE_CODE = os.getenv("SOURCE_CODE", "github.com/famouskaykay/djxkaykay")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION_NAME = os.getenv("SESSION_NAME")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
