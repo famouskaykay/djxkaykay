f3a86959aea74be603e0c77e80c2d82e88b0021b
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-28 14:54:28 +0000

    Update bot_utils.py

--- bot/helpers/bot_utils.py
+++ bot/helpers/bot_utils.py
@@ -1,7 +1,7 @@
-from bot import kay
+from bot import vcusr
 from bot.config import API_ID, API_HASH, BOT_TOKEN
 
-kay.start()
-ok = kay.get_me()
+vcusr.start()
+ok = vcusr.get_me()
 USERNAME = ok.username
 BOT_NAME = ok.first_name
