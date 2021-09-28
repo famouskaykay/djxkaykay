14236ce31ca664a55a1517ec66b1b0e593b7bca1
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-28 13:49:46 +0000

    Update bot_utils.py

--- bot/helpers/bot_utils.py
+++ bot/helpers/bot_utils.py
@@ -1,13 +1,13 @@
 from pyrogram import Client
 from bot.config import API_ID, API_HASH, BOT_TOKEN
 
-bot = Client(
+kay = Client(
     "VideoPlayer",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN
 )
-bot.start()
-ok = bot.get_me()
+kay.start()
+ok = kay.get_me()
 USERNAME = ok.username
 BOT_NAME = ok.first_name
