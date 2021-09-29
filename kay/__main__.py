6cd5a6db2e02e6729f1b5a211f197deaab7c28d6
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-29 14:09:05 +0000

    Create __main__.py

---
+++ kay/__main__.py
@@ -0,0 +1,19 @@
+from kay import kaykay
+from kay import TOKEN, API_ID, API_HASH
+from pyrogram import Client
+import logging, os
+
+logging.basicConfig(level=logging.WARNING,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+Client(
+    "VC Streamer",
+    API_ID,
+    API_HASH,
+    bot_token=TOKEN,
+    plugins={'root': 'bot.plugins'}
+).start()
+os.system("echo 'Bot Started'")
+vcusr.run()
