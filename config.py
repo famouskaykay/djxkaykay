4a40383651d5298475523c6c909ef006a5586934
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-29 14:42:37 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,22 @@
-import os
+# PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
 
-from os import getenv
-from dotenv import load_dotenv
+# Please edit 'CONFIG = False' to 'CONFIG = True'
+CONFIG = False
 
-load_dotenv()
-admins = {}
-ADMIN = int(os.getenv('ADMIN',1917528355))
-CHANNEL = int(os.getenv('CHANNEL',12345))
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abeb98ae0f581e")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "djxkaykay")
-ASSISTANT_NAME = os.getenv("ASSISTANT_NAME", "famouskaykay2")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "KayAspirerProject")
-UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "xprograming")
-SOURCE_CODE = os.getenv("SOURCE_CODE", "github.com/famouskayky/djxkaykay")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+# Add your APP ID from my.telegram.org
+API_ID = 123456
+
+# Add your API HASH from my.telegram.org
+API_HASH = "xxxxxxxxxxxxxxxxxxx"
+
+# Read readme and generate string session. Then add that to here.
+SESSION = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# Add your bot token here
+TOKEN = "12345678:xxxxxxxxxxxxxxxxxxxxxxxx"
+
+# Chat id where bot should work.
+CHAT_ID = -10010000000
+
+# Add ur TG ID, Seperate by spaces
+ADMINS = 1970289371
