0fd375efb2cc628f9488a4cba0f99f22adb87973
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-11-22 22:01:14 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ if os.path.exists("local.env"):
 load_dotenv()
 #ADMINS = 1970289371
 admins = {}
+
+SESSION = getenv("SESSION_NAME", "session")
+TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+CHAT_ID = getenv("GROUP_SUPPORT", "kayaspirerproject")
