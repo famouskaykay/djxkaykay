d89add17c8b30bb2b380630fd69e47ce7fd41f43
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-11-22 21:44:19 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,10 @@
-# PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
+import os
+from os import getenv
+from dotenv import load_dotenv
 
-# Please edit 'CONFIG = False' to 'CONFIG = True'
-CONFIG = False
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
-# Add your APP ID from my.telegram.org
-API_ID = 123456
-
-# Add your API HASH from my.telegram.org
-API_HASH = "xxxxxxxxxxxxxxxxxxx"
-
-# Read readme and generate string session. Then add that to here.
-SESSION = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-
-# Add your bot token here
-TOKEN = "12345678:xxxxxxxxxxxxxxxxxxxxxxxx"
-
-# Chat id where bot should work.
-CHAT_ID = -10010000000
-
-# Add ur TG ID, Seperate by spaces
-ADMINS = 1970289371
+load_dotenv()
+#ADMINS = 1970289371
+admins = {}
